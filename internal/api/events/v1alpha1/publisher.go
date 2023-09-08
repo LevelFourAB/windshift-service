4ec106e0d1a0859043ef734a74b3942c33aebb2b
Andreas Holstenson <a@holstenson.se>
2023-09-08 12:08:15 +0200

    chore(events): Context canceling while publishing should not report error
    This change makes it so that the gRPC server does not log an error when
    a publish fails due to the context being canceled, as this is usually
    bcause the client is disconnecting or the server is being shut down.

--- internal/api/events/v1alpha1/publisher.go
+++ internal/api/events/v1alpha1/publisher.go
@@ -2,10 +2,14 @@ package v1alpha1
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"windshift/service/internal/events"
 	eventsv1alpha1 "windshift/service/internal/proto/windshift/events/v1alpha1"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (e *EventsServiceServer) PublishEvent(ctx context.Context, req *eventsv1alpha1.PublishEventRequest) (*eventsv1alpha1.PublishEventResponse, error) {
@@ -31,6 +35,10 @@ func (e *EventsServiceServer) PublishEvent(ctx context.Context, req *eventsv1alp
 
 	ack, err := e.events.Publish(ctx, config)
 	if err != nil {
+		if errors.Is(err, context.Canceled) {
+			return nil, status.Error(codes.Canceled, "context canceled")
+		}
+
 		return nil, err
 	}
 	return &eventsv1alpha1.PublishEventResponse{
